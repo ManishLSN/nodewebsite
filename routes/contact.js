4bb4c30196da93ff7b82f9d808f2113e292023ea
Manish Jain <mjain@lsquared.com>
2018-03-21 14:14:46 +0530

    second commit for udpate contact

--- routes/contact.js
+++ routes/contact.js
@@ -15,14 +15,15 @@ router.post('/send', function(req, res, next) {
     var transporter = nodemailer.createTransport({
         service: 'GMail',
         auth: {
+			// REPLACE with original email and password and it will work (Verified - MJ)
             user: 'techguyinfo@gmail.com',
             pass: 'something'
         }
     });
 
     var mailOptions = {
-        from: 'Example <example@example.com>',
-        to: 'example@example.com',
+        from: 'LSQUARED <hub@lsquared.com>',
+        to: 'mjain@lsquared.com',
         subject: 'Website Submission',
         text: 'You have a new submission with the following details... Name: ' + req.body.name + ', Email: ' + req.body.email + ' Message: ' +req.body.message ,
         html: '<p>You got a new submission with the following details...</p><ul><li>Name: ' + req.body.name + '</li><li>Email: '+ req.body.email +'</li><li>Message: '+ req.body.message +'</li></ul>'
@@ -30,9 +31,12 @@ router.post('/send', function(req, res, next) {
 
     transporter.sendMail(mailOptions, function(error, info){
         if (error) {
+			alert('something wrnt wrong');
             console.log(error);
             res.redirect('/');
         } else {
+			alert('success got');
+			alert(info);
             console.log('Message Sent: ' + info.response);
             res.redirect('/');
         }
